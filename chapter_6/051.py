fd9442b552a0a0bbfbb33904ca242e31ddbdb6c1
YuitoTakeuchi-git <kureo3102@gmail.com>
2025-12-10 12:49:47 +0900

    done_050,051

---
+++ chapter_6/051.py
@@ -0,0 +1,16 @@
+from gensim.models import KeyedVectors
+import os
+
+model_path = '/home/takeuchi/workspace/nlp_100_nocks/chapter_6/GoogleNews-vectors-negative300.bin'
+model = KeyedVectors.load_word2vec_format(model_path, binary=True)
+
+results = model.most_similar('United_States', topn=10)
+
+os.makedirs('out', exist_ok=True)
+
+output_path = 'out/results_log_51.txt'
+with open(output_path, 'w', encoding='utf-8') as f:
+    for word, similarity in results:
+        f.write(f'{word}\t{similarity:.4f}\n')
+
+print('結果を保存しました。')
